4ec4593a5118549b8c6cd13839ed72795e6501e3
Fauzyah Hadirahma <97279376+fauzyahadirahma@users.noreply.github.com>
2022-01-26 01:48:44 +0700

    Latihan 1, 27 Desember 2021

---
+++ main.c
@@ -0,0 +1,24 @@
+/* Penulis : Fauzyah Hadirahma, email fauzyah.hadirahma@gmail.com */
+/* Latihan1 ngide aja wkwk*/
+
+#include <stdio.h>
+int main() {
+    /* KAMUS */
+    int p = 9;
+    int l = 10;
+    float a = 5.5;
+    float t = 10.5;
+    
+    /* ALGORITMA */
+    printf ("Menghitung Luas Persegi Panjang \n");
+    printf ("Panjang = %d \n", p);
+    printf ("Lebar   = %d \n", l);
+    printf ("Luas    = %d \n \n", p*l);
+    
+    printf ("Menghitung Luas Segitiga \n");
+    printf ("alas   = %f \n", a);
+    printf ("tinggi = %f \n", t);
+    printf ("Luas   = %f", a*t/2);
+    
+    
+}
